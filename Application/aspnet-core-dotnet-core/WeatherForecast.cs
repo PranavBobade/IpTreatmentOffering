80681949966fdad374a53f5bcf6ab42307853f50
Pranav Bobade <pranavtb99@gmail.com>
2021-07-03 20:24:49 +0530

    Updated From Vs with controller

---
+++ Application/aspnet-core-dotnet-core/WeatherForecast.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace aspnet_core_dotnet_core
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
